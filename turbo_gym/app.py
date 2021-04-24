33de22681b4691bccc492e45ca38fcc1798c82c9
MistaRae <itsamistarae@gmail.com>
2021-04-24 23:29:07 +0100

    app.py file populated

--- turbo_gym/app.py
+++ turbo_gym/app.py
@@ -1,3 +1,25 @@
-# from flask import Flask, render_template
+# imports
 
-# from controllers.booking_controller import bookings blu
+from flask import Flask, render_template
+# import blueprints
+
+# from controllers.booking_controller import bookings_blueprint
+# from controllers.lesson_controller import lessons_blueprint
+# from controllers.member_controller import members_blueprint
+# from controllers.slot_controller import slots_blueprint
+
+app = Flask(__name__)
+
+# register blueprints
+
+# app.register_blueprint(bookings_blueprint)
+# app.register_blueprint(lessons_blueprint)
+# app.register_blueprint(members_blueprint)
+# app.register_blueprint(slots_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

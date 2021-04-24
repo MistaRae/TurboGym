4df265a5578a46f4af7a3c5de86771673fd52bcb
MistaRae <itsamistarae@gmail.com>
2021-04-24 14:50:42 +0100

    renamed dbname in run_sql

--- turbo_gym/db/turbo_gym.sql
+++ turbo_gym/db/turbo_gym.sql
@@ -6,7 +6,7 @@ def run_sql(sql, values = None):
     results = []
 
     try:
-        conn=psycopg2.connect("dbname='quest_advisor'")
+        conn=psycopg2.connect("dbname='TurboGym'")
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()

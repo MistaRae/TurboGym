61818aeefd05e711ffd5298a7137b57f382a423d
MistaRae <itsamistarae@gmail.com>
2021-04-24 23:02:48 +0100

    tables defined

--- turbo_gym/db/turbo_gym.sql
+++ turbo_gym/db/turbo_gym.sql
@@ -4,21 +4,34 @@ DROP TABLE IF EXISTS slots;
 DROP TABLE IF EXISTS members;
 
 CREATE TABLE members (
-    id SERIAL PRIMARY KEY 
-
+    id SERIAL PRIMARY KEY, 
+    first_name VARCHAR(255),
+    Last_name VARCHAR(255),
+    age INT,
+    sex VARCHAR(255),
+    turbo_membership BOOLEAN,
+    active BOOLEAN
 );
 
 CREATE TABLE slots (
-     id SERIAL PRIMARY KEY 
-    
+    id SERIAL PRIMARY KEY, 
+    slot_num INT,
+    time_stamp VARCHAR(255),
+    turbo_slot BOOLEAN
 );
 
 CREATE TABLE lessons (
-     id SERIAL PRIMARY KEY 
-    
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255),
+    class_type VARCHAR(255),
+    difficulty VARCHAR(255),
+    duration INT,
+    capacity INT,
+    slot_id INT REFERENCES slots(id) ON DELETE CASCADE   
 );
 
 CREATE TABLE bookings (
-     id SERIAL PRIMARY KEY 
-    
+    id SERIAL PRIMARY KEY,
+    member_id INT REFERENCES members(id),
+    lesson_id INT REFERENCES lessons(id)    
 );
--- turbo_gym/models/slot.py
+++ turbo_gym/models/slot.py
@@ -1,6 +1,10 @@
 class Slot:
 
-    def __init__(self, slot_num, time_stamp, id = None):
+    def __init__(self, slot_num, time_stamp, turbo_slot=None, id = None):
       self.slot_num = slot_num
       self.time_stamp = time_stamp
+      self.turbo_slot = turbo_slot
       self.id = id
+
+
+# no methods

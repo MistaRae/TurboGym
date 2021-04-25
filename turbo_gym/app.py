0e0c6cb23b71e68c2ed3e9a567bed6b0f6ee59e2
MistaRae <itsamistarae@gmail.com>
2021-04-25 23:59:19 +0100

    fixed render template issue

--- turbo_gym/app.py
+++ turbo_gym/app.py
@@ -1,6 +1,7 @@
 # imports
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 # import blueprints
 
 # from controllers.booking_controller import bookings_blueprint
@@ -17,9 +18,9 @@ app = Flask(__name__)
 # app.register_blueprint(members_blueprint)
 # app.register_blueprint(slots_blueprint)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def main():
+    return render_template("index.html",)
 
 if __name__ == '__main__':
     app.run(debug=True)

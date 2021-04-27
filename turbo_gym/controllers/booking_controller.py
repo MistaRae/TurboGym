1adb84b5357d2d3ab40743d1e65afdc9f30ca4e6
MistaRae <itsamistarae@gmail.com>
2021-04-27 15:13:57 +0100

    bookings now being created, but not displayed

--- turbo_gym/controllers/booking_controller.py
+++ turbo_gym/controllers/booking_controller.py
@@ -5,6 +5,7 @@ from models.booking import Booking
 import repositories.booking_repository as booking_repository
 import repositories.lesson_repository as lesson_repository
 import repositories.member_repository as member_repository
+import repositories.slot_repository as slot_repository
 
 bookings_blueprint = Blueprint("bookings", __name__)
 
@@ -17,16 +18,20 @@ def bookings():
 
 @bookings_blueprint.route('/classes/bookings/<lesson_id>', methods= ['POST']) 
 def new_booking(lesson_id):
+    slots = slot_repository.select_all()
     lesson = lesson_repository.select(lesson_id)
     members = member_repository.select_all()
-    return render_template('/bookings/new.html', lesson = lesson, members = members)
+    return render_template('/bookings/new.html', lesson = lesson, members = members, slots = slots)
 
-# @bookings_blueprint.route('/bookings/new')
-    
-
-# @bookings_blueprint.route('/bookings/new')
-# def create_booking():
-#     member_id = request.form()
+@bookings_blueprint.route('/bookings/new', methods=['POST'])
+def create_booking():
+    member_id = request.form['member_id']
+    lesson_id = request.form['lesson_id']
+    member = member_repository.select(member_id)
+    lesson = lesson_repository.select(lesson_id)
+    booking = Booking(member, lesson)
+    booking_repository.save(booking)
+    return redirect('/bookings')
 
 
 
